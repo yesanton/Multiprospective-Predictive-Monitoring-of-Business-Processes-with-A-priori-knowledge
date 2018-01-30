a606c8d8cfab2d250d1dd845dfce6889f420eb05
kaurjvpld <kaurjarvpold@gmail.com>
2018-01-30 14:41:50 +0100

    train model with the group attribute

--- src/experiments_runner.py
+++ src/experiments_runner.py
@@ -1,9 +1,9 @@
-'''
+"""
 This file was created to manage running of experiments.
 
 ## settings are in the shared variables file
 Author; Anton Yeshchenko
-'''
+"""
 
 
 # This will run whole set of experiments
@@ -12,23 +12,24 @@ from inference_algorithms import _11_cycl_pro_SUFFIX_only
 from inference_algorithms import _6_evaluate_beseline_SUFFIX_only
 from inference_algorithms import _9_cycl_SUFFIX_only
 from shared_variables import activateSettings
-from train import train
+# from train import train
+from train_group import train
 
 formula1 = "WEAK"
 formula2 = "STRONG"
 
 formula_used = formula1
 
-logNumber = 7
+logNumber = 8
 
-train()
+# train()
 
 # _6_evaluate_beseline_SUFFIX_only.runExperiments()
 # _9_cycl_SUFFIX_only.py.runExperiments()
 # _10_cycl_back_SUFFIX_only.runExperiments()
 
 
-# _6_evaluate_beseline_SUFFIX_only.runExperiments(logNumber,formula_used)
+_6_evaluate_beseline_SUFFIX_only.runExperiments(logNumber, formula_used)
 # _9_cycl_SUFFIX_only.runExperiments(logNumber,formula_used)
 # _10_cycl_back_SUFFIX_only.runExperiments(logNumber,formula_used)
 # _11_cycl_pro_SUFFIX_only.runExperiments(logNumber,formula_used)
--- src/support_scripts/csv_converter_extra.py
+++ src/support_scripts/csv_converter_extra.py
@@ -5,7 +5,7 @@ import time
 import datetime
 
 eventlog_in = "bpi_2014_detail_incident_non_processed.csv"
-eventlog_out = "bpi_14_detail_incident_extra.csv"
+eventlog_out = "bpi_14_detail_incident_extra2.csv"
 
 CASE_ID = 0
 ACTIVITY_ID = 3
@@ -18,38 +18,40 @@ spamreader = csv.reader(csvfile_in, delimiter=';', quotechar='|')
 next(spamreader, None)  # skip the headers
 
 dictionary = {}
-#dictionary["a"] = 1
+dictionary_group = {}
+# dictionary["a"] = 1
 
 give_number = 0
+give_number_group = 0
 
 
 with open('%s' % eventlog_out, 'wb') as csvfile_out:
     writer = csv.writer(csvfile_out, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(["CaseID","ActivityID","CompleteTimestamp","GroupID"])
+    writer.writerow(["CaseID", "ActivityID", "CompleteTimestamp", "GroupID"])
 
     mark = True
 
     current_event = 0
     for row in spamreader:
-        timestamp = row[TIMESTAMP_ID] #timestamp in hospital log
-  #      split_time = timestamp.split("T")
-
- #       timestamp = split_time[0]+" "+ split_time[1].split("+")[0]
-
-        timestamp = datetime.datetime.strptime(timestamp, "%d-%m-%Y %H:%M:%S") #.%f")
-
+        timestamp = row[TIMESTAMP_ID]  # timestamp in hospital log
+#       split_time = timestamp.split("T")
+#       timestamp = split_time[0]+" "+ split_time[1].split("+")[0]
+        timestamp = datetime.datetime.strptime(timestamp, "%d-%m-%Y %H:%M:%S")  # .%f")
         timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
         if row[ACTIVITY_ID] not in dictionary:
             dictionary[row[ACTIVITY_ID]] = give_number
             give_number = give_number + 1
 
+        if row[GROUP_ID] not in dictionary_group:
+            dictionary_group[row[GROUP_ID]] = give_number_group
+            give_number_group = give_number_group + 1
 
         output = []
         output.append(row[CASE_ID])
         output.append(dictionary[row[ACTIVITY_ID]])
         output.append(timestamp)
-        output.append(row[GROUP_ID])
+        output.append(dictionary_group[row[GROUP_ID]])
 
         if mark:
             mark = False
@@ -62,11 +64,3 @@ with open('%s' % eventlog_out, 'wb') as csvfile_out:
             trace_save = [output]
         else:
             trace_save.append(output)
-
-
-
-
-
-
-
-
