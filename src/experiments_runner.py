b5214ab60150b58f61a3c83471c9d92822e0a5b6
Anton Yeshchenko <yeshchenko@anton.ai.wu.ac.at>
2018-07-16 00:33:20 +0000

    merge fix

--- src/experiments_runner.py
+++ src/experiments_runner.py
@@ -12,7 +12,7 @@ from inference_algorithms import _6_evaluate_baseline_SUFFIX_and_group
 from inference_algorithms import _11_cycl_pro_SUFFIX_resource_LTL
 from inference_algorithms import _11_cycl_pro_SUFFIX_declare_smart_queue
 
-from shared_variables import activateSettings, eventlog
+from shared_variables import eventlog, activate_settings
 from train import train
 from train_with_data import train_with_data
 
